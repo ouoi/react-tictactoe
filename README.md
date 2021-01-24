24d74d221ea0cc23dadaee9d0ac79a2ab192e190
ouoi <ouoiouoi@gmail.com>
2021-01-24 18:08:48 +0900

    project init

---
+++ README.md
@@ -0,0 +1,5 @@
+# 자습서: React 시작하기
+
+React Document의 Tictactoe Tutorial
+
+https://ko.reactjs.org/tutorial/tutorial.html
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './components/Game';
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
